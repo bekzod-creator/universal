0cc7a8de27224f3782e908293b37ab662df3deb1
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-10-05 00:52:02 +0500

    Add files via upload

---
+++ tapswap.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        tapswap getter 
+// @namespace   Violentmonkey Scripts
+// @match       *://*.tapswap.*/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 10/5/2024, 12:48:38 AM
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function clickGetItButton(attempts) {
+        const button = document.querySelector('button._button_fffa0_1._secondary_fffa0_21._large_fffa0_49._fullWidth_fffa0_37');
+        if (button) {
+            button.click();
+            console.log('Button clicked!');
+        } else if (attempts > 0) {
+            console.log('Button not found, retrying...');
+            setTimeout(() => clickGetItButton(attempts - 1), 1000); // Wait 1 second before retrying
+        } else {
+            console.log('Button not found after 3 attempts.');
+        }
+    }
+
+    // Start the process with 3 attempts
+    clickGetItButton(3);
+})();
