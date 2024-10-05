3a761499019a854166687b7c56d759c875a88d74
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-10-05 08:51:35 +0500

    Update memefienabler.user.js

--- memefienabler.user.js
+++ memefienabler.user.js
@@ -5,8 +5,8 @@
 // @grant        none
 // @version      1.2
 // @author       bekzod
-// @downloadURL  https://github.com/bekzod-creator/universal/raw/refs/heads/main/memefi%20enabler.user.js
-// @updateURL    https://raw.githubusercontent.com/bekzod-creator/universal/refs/heads/main/memefi enabler.user.js
+// @downloadURL  https://github.com/bekzod-creator/universal/raw/refs/heads/main/memefienabler.user.js
+// @updateURL    https://github.com/bekzod-creator/universal/raw/refs/heads/main/memefienabler.user.js
 // @description  f you
 // ==/UserScript==
 
