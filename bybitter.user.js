6e0588ca8439893d9e295aafe745371f13943886
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-10-08 01:27:40 +0500

    Create bybitter.user.js

---
+++ bybitter.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Patch Bybit Coinsweeper
+// @namespace    Violentmonkey Scripts
+// @match        *://bybitcoinsweeper.com/*
+// @grant        none
+// @version      0.1
+// @author       zoder
+// @icon         https://mudachyo.codes/bybit/logo.jpg
+// @downloadURL  -
+// @updateURL    -
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function clickPlayAgain() {
+        // Try to find the "Play Again" button
+        let playButton = document.querySelector('button.btn.primary-btn');
+
+        if (playButton && playButton.textContent.trim() === 'Play Again') {
+            // Button found, click it
+            console.log("Found 'Play Again' button, clicking...");
+            playButton.click();
+        } else {
+            // Button not found, retry after 10 seconds
+            console.log("'Play Again' button not found, retrying in 10 seconds...");
+            setTimeout(clickPlayAgain, 10000); // Retry after 10 seconds
+        }
+    }
+
+    // Start the checking process
+    clickPlayAgain();
+})();
