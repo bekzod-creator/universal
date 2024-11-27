475f4c0866c6ff13d3e38050a6fd01a1cc660cb6
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-11-27 22:58:51 +0500

    Update auto.user.js

--- auto.user.js
+++ auto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         tanagram AI
 // @namespace    http://tampermonkey.net/
-// @version      2.8
+// @version      2.9
 // @description  Automatically open Telegram bots, click the Play button, then click the Launch button (if available), and move to the next bot with page refresh, then repeat the cycle infinitely.
 // @author       zoder codes
 // @downloadURL  https://raw.githubusercontent.com/bekzod-creator/universal/main/auto.user.js
