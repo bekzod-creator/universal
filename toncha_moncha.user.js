2a918cc2ad55c79fac5467f8d0d0277a81e876b7
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-12-03 01:11:41 +0500

    Update toncha_moncha.user.js

--- toncha_moncha.user.js
+++ toncha_moncha.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Toncha moncha webcha
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  toncha moncha to webcha
 // @author       Your Name
 // @match        https://tonstation.app/app/*
