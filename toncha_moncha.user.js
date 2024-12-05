fa607916aa63c09c669e43ed475d991632d4a4e2
DevOps <64070963+bekzod-creator@users.noreply.github.com>
2024-12-05 22:19:32 +0500

    Update toncha_moncha.user.js

--- toncha_moncha.user.js
+++ toncha_moncha.user.js
@@ -11,70 +11,58 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
+ /$$$$$$ /$$$$$$$$       /$$     /$$ /$$$$$$  /$$   /$$        /$$$$$$  /$$$$$$$  /$$$$$$$$       /$$   /$$  /$$$$$$  /$$$$$$$$       /$$$$$$$   /$$$$$$  /$$   /$$  /$$$$$$  /$$$$$$$$ /$$$$$$$$       /$$      /$$ /$$$$$$$$       /$$     /$$ /$$$$$$  /$$   /$$        /$$$$$$  /$$$$$$$  /$$$$$$$$        /$$$$$$   /$$$$$$  /$$     /$$
+|_  $$_/| $$_____/      |  $$   /$$//$$__  $$| $$  | $$       /$$__  $$| $$__  $$| $$_____/      | $$$ | $$ /$$__  $$|__  $$__/      | $$__  $$ /$$__  $$| $$$ | $$ /$$__  $$|__  $$__/| $$_____/      | $$$    /$$$| $$_____/      |  $$   /$$//$$__  $$| $$  | $$       /$$__  $$| $$__  $$| $$_____/       /$$__  $$ /$$__  $$|  $$   /$$/
+  | $$  | $$             \  $$ /$$/| $$  \ $$| $$  | $$      | $$  \ $$| $$  \ $$| $$            | $$$$| $$| $$  \ $$   | $$         | $$  \ $$| $$  \ $$| $$$$| $$| $$  \ $$   | $$   | $$            | $$$$  /$$$$| $$             \  $$ /$$/| $$  \ $$| $$  | $$      | $$  \ $$| $$  \ $$| $$            | $$  \__/| $$  \ $$ \  $$ /$$/ 
+  | $$  | $$$$$           \  $$$$/ | $$  | $$| $$  | $$      | $$$$$$$$| $$$$$$$/| $$$$$         | $$ $$ $$| $$  | $$   | $$         | $$  | $$| $$  | $$| $$ $$ $$| $$$$$$$$   | $$   | $$$$$         | $$ $$/$$ $$| $$$$$           \  $$$$/ | $$  | $$| $$  | $$      | $$$$$$$$| $$$$$$$/| $$$$$         | $$ /$$$$| $$$$$$$$  \  $$$$/  
+  | $$  | $$__/            \  $$/  | $$  | $$| $$  | $$      | $$__  $$| $$__  $$| $$__/         | $$  $$$$| $$  | $$   | $$         | $$  | $$| $$  | $$| $$  $$$$| $$__  $$   | $$   | $$__/         | $$  $$$| $$| $$__/            \  $$/  | $$  | $$| $$  | $$      | $$__  $$| $$__  $$| $$__/         | $$|_  $$| $$__  $$   \  $$/   
+  | $$  | $$                | $$   | $$  | $$| $$  | $$      | $$  | $$| $$  \ $$| $$            | $$\  $$$| $$  | $$   | $$         | $$  | $$| $$  | $$| $$\  $$$| $$  | $$   | $$   | $$            | $$\  $ | $$| $$                | $$   | $$  | $$| $$  | $$      | $$  | $$| $$  \ $$| $$            | $$  \ $$| $$  | $$    | $$    
+ /$$$$$$| $$                | $$   |  $$$$$$/|  $$$$$$/      | $$  | $$| $$  | $$| $$$$$$$$      | $$ \  $$|  $$$$$$/   | $$         | $$$$$$$/|  $$$$$$/| $$ \  $$| $$  | $$   | $$   | $$$$$$$$      | $$ \/  | $$| $$$$$$$$          | $$   |  $$$$$$/|  $$$$$$/      | $$  | $$| $$  | $$| $$$$$$$$      |  $$$$$$/| $$  | $$    | $$    
+|______/|__/                |__/    \______/  \______/       |__/  |__/|__/  |__/|________/      |__/  \__/ \______/    |__/         |_______/  \______/ |__/  \__/|__/  |__/   |__/   |________/      |__/     |__/|________/          |__/    \______/  \______/       |__/  |__/|__/  |__/|________/       \______/ |__/  |__/    |__/    
+                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                                                                                             
 
-    // Function to update iframe src attributes and return the updated src
-    function updateIframeSrc() {
-        const iframes = document.querySelectorAll('iframe[src*="=web"]'); // Find iframes containing "=web"
-        let updatedSrc = '';
-        iframes.forEach(iframe => {
-            const src = iframe.getAttribute('src');
-            updatedSrc = src.replace('=web', '=ios'); // Replace "=web" with "=ios" (or another string as needed)
-            iframe.setAttribute('src', updatedSrc);
-            console.log(`Updated iframe src to: ${updatedSrc}`);
-        });
-        return updatedSrc; // Return the last updated src value
-    }
+TON: UQBVtrnw4jovxNedCM4UsnzKazM7M5Dm9-4g8oDu7H70AmXa
 
-    // Function to open the updated src in a mini window centered on the screen and zoomed to 50%
-    function openTargetLinkInMiniWindow(updatedSrc) {
-        // Window dimensions
-        const width = 400;
-        const height = 600;
+USDT (TRC20): TVFC9iJ9bTbaPo12vzqGidRfVaSy4h8znR
 
-        // Calculate the center of the screen
-        const left = (window.innerWidth / 2) - (width / 1);
-        const top = (window.innerHeight / 2) - (height / 2);
+BTC:  1GSmspH1XjDJVWHr2QfjdCfxeFFbeekNTn
 
-        // Open a new mini window with the calculated position and size
-        const miniWindow = window.open(updatedSrc, '_blank', `width=${width},height=${height},left=${left},top=${top}`);
 
-        console.log(`Opening updated src in a mini window: ${updatedSrc}`);
 
-        // Apply CSS zoom to scale the content to 50%
-        miniWindow.document.body.style.transform = 'scale(0.5)';
-        miniWindow.document.body.style.transformOrigin = '0 0'; // Ensure the top-left corner stays fixed
 
-        // Close the mini window after 10 seconds
-        setTimeout(() => {
-            if (miniWindow && !miniWindow.closed) {
-                miniWindow.close();
-                console.log('Closed the mini window after 10 seconds.');
-            }
-        }, 20000); // 10 seconds delay
-    }
+  /$$$$$$  /$$$$$$$        /$$$$$$$$ /$$$$$$  /$$   /$$ /$$$$$$$$       /$$$$$$$$ /$$   /$$ /$$$$$$  /$$$$$$       
+ /$$__  $$| $$__  $$      |__  $$__//$$__  $$| $$  /$$/| $$_____/      |__  $$__/| $$  | $$|_  $$_/ /$$__  $$      
+| $$  \ $$| $$  \ $$         | $$  | $$  \ $$| $$ /$$/ | $$               | $$   | $$  | $$  | $$  | $$  \__/      
+| $$  | $$| $$$$$$$/         | $$  | $$$$$$$$| $$$$$/  | $$$$$            | $$   | $$$$$$$$  | $$  |  $$$$$$       
+| $$  | $$| $$__  $$         | $$  | $$__  $$| $$  $$  | $$__/            | $$   | $$__  $$  | $$   \____  $$      
+| $$  | $$| $$  \ $$         | $$  | $$  | $$| $$\  $$ | $$               | $$   | $$  | $$  | $$   /$$  \ $$      
+|  $$$$$$/| $$  | $$         | $$  | $$  | $$| $$ \  $$| $$$$$$$$         | $$   | $$  | $$ /$$$$$$|  $$$$$$/      
+ \______/ |__/  |__/         |__/  |__/  |__/|__/  \__/|________/         |__/   |__/  |__/|______/ \______/       
+                                                                                                                   
+                                                                                                                   
+                                                                                                                   
 
-    // Initial update when the script runs
-    const updatedSrc = updateIframeSrc();
-    if (updatedSrc) {
-        openTargetLinkInMiniWindow(updatedSrc); // Open the updated src in the mini window
-    }
 
-    // Observe DOM changes to apply updates dynamically
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach(() => {
-            const updatedSrc = updateIframeSrc(); // Get updated src after mutation
-            if (updatedSrc) {
-                openTargetLinkInMiniWindow(updatedSrc); // Open the updated src in the mini window
-            }
-        });
-    });
-
-    // Observer settings to detect changes in DOM
-    const config = {
-        childList: true,
-        subtree: true
-    };
-    observer.observe(document.body, { childList: true, subtree: true });
-})();
+⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣴⣿⣿⠿⣟⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢸⣏⡏⠀⠀⠀⢣⢻⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢸⣟⠧⠤⠤⠔⠋⠀⢿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣿⡆⠀⠀⠀⠀⠀⠸⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⣿⡀⢀⣶⠤⠒⠀⢻⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠈⢿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣿⡆⠀⠀⠀⠀⠀⠈⢿⣆⣠⣤⣤⣤⣤⣴⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⢿⢿⠀⠀⠀⢀⣀⣀⠘⣿⠋⠁⠀⠙⢇⠀⠀⠙⢿⣦⡀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣾⢇⡞⠘⣧⠀⢖⡭⠞⢛⡄⠘⣆⠀⠀⠀⠈⢧⠀⠀⠀⠙⢿⣄⠀⠀⠀⠀
+⠀⠀⣠⣿⣛⣥⠤⠤⢿⡄⠀⠀⠈⠉⠀⠀⠹⡄⠀⠀⠀⠈⢧⠀⠀⠀⠈⠻⣦⠀⠀⠀
+⠀⣼⡟⡱⠛⠙⠀⠀⠘⢷⡀⠀⠀⠀⠀⠀⠀⠹⡀⠀⠀⠀⠈⣧⠀⠀⠀⠀⠹⣧⡀⠀
+⢸⡏⢠⠃⠀⠀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠘⣧⠀⠀⠀⠀⠸⣷⡀
+⠸⣧⠘⡇⠀⠀⠀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀⠀⢹⡇⠀⠀⠀⠀⣿⠇
+⠀⣿⡄⢳⠀⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⣼⡟⠀
+⠀⢹⡇⠘⣇⠀⠀⠀⠀⠀⠀⠰⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⣼⡟⠀⠀
+⠀⢸⡇⠀⢹⡆⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⢳⣼⠟⠀⠀⠀
+⠀⠸⣧⣀⠀⢳⡀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⢃⠀⢀⣴⡿⠁⠀⠀⠀⠀
+⠀⠀⠈⠙⢷⣄⢳⡀⠀⠀⠀⠀⠀⠀⢳⡀⠀⠀⠀⠀⠀⣠⡿⠟⠛⠉⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠈⠻⢿⣷⣦⣄⣀⣀⣠⣤⠾⠷⣦⣤⣤⡶⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
